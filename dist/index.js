13821e09d94a60460dd9cd7330a23f4e545e35fd
Action workflow <dave@digitalnoise.net>
2023-07-03 22:33:17 +0000

    dist created from refs/heads/feat/AddVulnAlerts for sha (35c2edc71bdfcf526a53ec5900c146222bfacb73)

--- dist/index.js
+++ dist/index.js
@@ -14520,6 +14520,9 @@ const getVulnerabilityAlerts = async (org, repo) => {
 
 		let getVulnResult = null;
 
+		console.log(`${indent.join('')}${org}/${repo}: Querying GraphQL for vulnerabilities...`);
+
+
 		try {
 			getVulnResult = await graphql({ query, org: org, repo: repo, cursor: pagination });
 		}
@@ -14535,6 +14538,8 @@ const getVulnerabilityAlerts = async (org, repo) => {
 
 		hasNextPage = getVulnResult.repository.vulnerabilityAlerts.pageInfo.hasNextPage;
 		const repoVulnerabilities = getVulnResult.repository.vulnerabilityAlerts.nodes;
+
+		console.log(`vulnerabilities: ${repoVulnerabilities}`);
 
 		for (const repoVulnerability of repoVulnerabilities) {
 			console.log(`${indent.join('')}${org}/${repo}: ${repoVulnerability.securityVulnerability.package.name} vulnerability found with severity ${repoVulnerability.securityVulnerability.severity}.`)
